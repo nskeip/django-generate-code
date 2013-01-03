a36e7426e0c73f6e5a04b965817af198abbb5b0d
Nikita Hismatov <hismatovns@gmail.com>
2013-01-04 02:15:44 +0600

    a little example

--- management/commands/g.py
+++ management/commands/g.py
@@ -1,6 +1,19 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
+    """
+    G example:
+
+    python manage.py g model App.MyModel title:string price:decimal \
+     description:text
+
+    Will add to app/models.py code like this:
+
+    class MyModel(models.Model):
+        title = models.CharField(min_length=255)
+        price = models.DecimalField(max_digits=9, decimal_places=2)
+        description = models.TextField()
+    """
     help = 'Generates useful code for you'
 
     def handle(self, *args, **kwargs):
