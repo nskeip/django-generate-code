07a524e4502ff0bf0e3c79c5daedbb2ee06b4c54
Nikita Hismatov <hismatovns@gmail.com>
2013-01-04 02:06:33 +0600

    added g.py

---
+++ management/commands/g.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Generates useful code for you'
+
+    def handle(self, *args, **kwargs):
+        pass
