09a2c6dfd33d468076201fe0aff99bae3e205611
Nikita Hismatov <hismatovns@gmail.com>
2013-01-04 18:31:27 +0600

    setup.py (needs fixing)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='django-generate-code',
+    version='0.1.0',
+    description='django-generate-code lets you generate code '
+                'in your django projects straight from the '
+                'command line',
+    packages=['django_generate_code']
+)
